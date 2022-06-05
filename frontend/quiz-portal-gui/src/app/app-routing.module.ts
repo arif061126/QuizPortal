bf0677b8aefca04968ad898e810142185bfe5567
a.ahmed <a.ahmed@fz-juelich.de>
2022-06-05 10:41:34 +0200

    add routing frontend angular

--- frontend/quiz-portal-gui/src/app/app-routing.module.ts
+++ frontend/quiz-portal-gui/src/app/app-routing.module.ts
@@ -1,8 +1,56 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {LoginComponent} from "./pages/login/login.component";
+import {SignupComponent} from "./pages/signup/signup.component";
+import {HomeComponent} from "./pages/home/home.component";
+import {AdminDashboardComponent} from "./pages/admin-dashboard/admin-dashboard.component";
+import {UserDashboardComponent} from "./pages/user-dashboard/user-dashboard.component";
+import {AdminGuard} from "./services/admin.guard";
+import {NormalGuard} from "./services/normal.guard";
+import {ProfileComponent} from "./pages/profile/profile.component";
+import {WelcomeComponent} from "./pages/welcome/welcome.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent,
+    pathMatch:'full'
+  },
+  {
+    path: 'signup',
+    component: SignupComponent,
+    pathMatch:'full'
+  },
+  {
+    path: 'admin-dashboard',
+    component: AdminDashboardComponent,
+    canActivate:[AdminGuard],
+    children:[
+      {
+        path:'welcome',
+        component:WelcomeComponent,
+        pathMatch:'full'
+      },
+      {
+        path:'profile',
+        component:ProfileComponent,
+        pathMatch:'full'
+      }
+    ]
+
+  },
+  {
+    path: 'user-dashboard',
+    component: UserDashboardComponent,
+    pathMatch:'full',
+    canActivate:[NormalGuard]
+  },
+  {
+    path: '',
+    component: HomeComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/quiz-portal-gui/src/app/app.module.ts
+++ frontend/quiz-portal-gui/src/app/app.module.ts
@@ -3,16 +3,67 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from "@angular/material/button";
+import {MatCardModule} from "@angular/material/card";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatIconModule} from "@angular/material/icon";
+import {MatInputModule} from "@angular/material/input";
+import {MatSnackBarModule} from "@angular/material/snack-bar";
+import {MatTableModule} from "@angular/material/table";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { LoginComponent } from './pages/login/login.component';
+import {FormsModule} from "@angular/forms";
+import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
+import { HomeComponent } from './pages/home/home.component';
+import {AuthInterceptor, authInterceptorProvider} from "./services/auth.interceptor";
+import {LoginService} from "./services/login.service";
+import {AuthGuard} from "./auth.guard";
+import { AdminDashboardComponent } from './pages/admin-dashboard/admin-dashboard.component';
+import { UserDashboardComponent } from './pages/user-dashboard/user-dashboard.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import {MatListModule} from "@angular/material/list";
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { WelcomeComponent } from './pages/welcome/welcome.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SignupComponent,
+    LoginComponent,
+    HomeComponent,
+    AdminDashboardComponent,
+    UserDashboardComponent,
+    ProfileComponent,
+    SidebarComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatSnackBarModule,
+    MatTableModule,
+    MatToolbarModule,
+    NgbModule,
+    FormsModule,
+    HttpClientModule,
+    MatListModule
   ],
-  providers: [],
+  providers: [
+    LoginService,
+    AuthGuard,
+    authInterceptorProvider
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
